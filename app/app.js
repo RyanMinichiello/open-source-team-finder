404994ab9cfe00ea5d6bb205c11157e1c9670b32
ryanminichiello <ryan.minichiello@gmail.com>
2017-03-26 21:28:25 -0400

    highest level components, nav

--- app/app.js
+++ app/app.js
@@ -2,25 +2,20 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 // Each major browser view user interface must be imported.
-import UI01 from './components/ui-01.js';
-import UI02 from './components/ui-02.js';
-import ProjectPage from './components/project-page.js'
+import ProjectPage from './components/project-page.js';
+import Profile_Temp from './components/profile_page/profile_temp.js';
 
 // For each view conditionally determine which view to display
 // depending on if the ID is present in the HTML.
-if (document.getElementById('ui-01') !== null) {
-  ReactDOM.render(
-    <UI01 />,
-    document.getElementById('ui-01')
-  );
-} else if (document.getElementById('ui-02') !== null) {
-  ReactDOM.render(
-    <UI02 />,
-    document.getElementById('ui-02')
-  );
-}else if (document.getElementById('project_page-zone') !== null) {
+
+ if (document.getElementById('project_page-zone') !== null) {
   ReactDOM.render(
     <ProjectPage />,
     document.getElementById('project_page-zone')
+  );
+}else if (document.getElementById('profile_temp') !== null) {
+  ReactDOM.render(
+    <Profile_Temp />,
+    document.getElementById('profile_temp')
   );
 }
