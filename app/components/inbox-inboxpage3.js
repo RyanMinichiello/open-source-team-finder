b0a95d04e7893578788bf8e7da6bd58a49a30ffd
Kyu Min Kim <kyumin0409@gmail.com>
2017-03-28 12:14:16 -0400

    fixed side bar and navbar on inbox pages

--- app/components/inbox-inboxpage3.js
+++ app/components/inbox-inboxpage3.js
@@ -2,15 +2,22 @@ import React from 'react';
 import MessageItems3 from './inbox-messageitems3';
 import InboxItems3 from './inbox-inboxitems3';
 import Profile_Sidebar from './profile_page/profile_sidebar';
-import Navbar from './navbar';
+import Profile_Nav from './profile_page/profile_nav';
 
 export default class InboxPage3 extends React.Component {
   render() {
     return (
     <div>
 
-      <Navbar />
-      <Profile_Sidebar />
+      <Profile_Nav
+          self_name = "Jane"
+          ></Profile_Nav>
+        <Profile_Sidebar
+            first_active = "Project 1"
+            second_active = "Project 2"
+            third_active = "Project 3"
+            fourth_active = "Project 4"
+            ></Profile_Sidebar>
 
         <div className = "col-md-2">
         </div>
