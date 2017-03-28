bd80b15360863c3fb9a175ccbb04bb6cc27f4592
ryan-lecours <rlecours@umass.edu>
2017-03-28 00:42:27 -0400

    Updated: added react components, fixed top margin issue, changed date input boxes
    TODO: make function to add more tags
          make function to add more positions

--- app/components/createproject.js
+++ app/components/createproject.js
@@ -1,6 +1,8 @@
 import React from 'react';
 
 
+import CreateProjectTagSection from './createproject-tag-section.js';
+import CreateProjectPositionSection from './createproject-position-list.js';
 
 export default class CreateProject extends React.Component {
   render() {
@@ -8,45 +10,39 @@ export default class CreateProject extends React.Component {
 
 
       <div>
-
-
-         <div className="col-md-4 leftmain">
-          <div id = "create-project-title" className= "attribute-box attribute">Create Project</div>
-          <div className= "attribute-box">
-          <div className= "attribute">Project Name:</div>
-          <input />
+        <div className="col-md-4 leftmain">
+         <div id = "create-project-title" className= "attribute-box attribute">Create Project</div>
+         <div className= "attribute-box">
+         <div className= "attribute">Project Name:</div>
+         <input />
         </div>
         <div className= "attribute-box">
-          <div className= "attribute">Description:</div>
-          <input />
+         <div className= "attribute">Description:</div>
+         <input />
         </div>
-        <div className= "attribute-box">
-          <div className= "attribute">Areas of Interest (5 Max):</div>
-          <input placeholder="Add Tags" />
+
+        <div className ="tag-section">
+          <CreateProjectTagSection />
         </div>
+
         <img id="logo" alt="OSTF Logo" src="img/logo_notext.png" height="auto" width="auto" />
         </div>
 
 
+
+
         <div className="col-md-4 rightmain">
-          <div className = "attribute-box" id="positions">
-          <div className = "attribute">Positions:</div>
-          <input />
-          <input />
-          <input />
-        </div>
-          <button type="button" id="add-more-btn" className="btn btn-default">
-          Add More...
-          </button>
+
+        <CreateProjectPositionSection />
 
           <div className = "attribute-box date">
-          <div className = "attribute">Start Date:
-          <input />
+          <div className = "attribute">Start Date: 
+          <input type="date" />
           </div>
         </div>
         <div className = "attribute-box date">
           <div className = "attribute">End Date:
-          <input />
+          <input type="date" />
           </div>
         </div>
 
