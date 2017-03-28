73864995ed8b59ba45268c0744201ee7dc587d65
lookup828 <tcloutier@umass.edu>
2017-03-28 16:44:05 -0400

    Project-Page Update
    NavBar Update

--- app/components/project-page.js
+++ app/components/project-page.js
@@ -3,6 +3,7 @@ import ProjectBanner from './project-banner';
 import ProjectDescription from './project-description';
 import ProjectMessages from './project-messages';
 import ProjectPositions from './project-positions';
+import Navbar from './navbar.js';
 
 export default class ProjectPage extends React.Component {
   render(){
