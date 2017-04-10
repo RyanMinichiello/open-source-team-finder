8c13f866f54497fdb9b862845f8cb8927870c3f2
josh-tranfaglia <jtranfaglia@umass.edu>
2017-04-10 11:42:18 -0400

    side bar and top navbar now passed as react components.

--- app/components/job-board/job-board.js
+++ app/components/job-board/job-board.js
@@ -1,10 +1,23 @@
 import React from 'react';
+import Navbar from '../navbar.js';
+import Sidebar from '../sidebar.js';
 import JobBoardMainFeed from './job-board-main-feed.js'
 
 export default class JobBoard extends React.Component {
   render() {
     return (
       <div>
+        <Navbar
+          self_name = "Jane"
+          ></Navbar>
+          <div>
+            <Sidebar
+              first_active = "Project 1"
+              second_active = "Project 2"
+              third_active = "Project 3"
+              fourth_active = "Project 4"
+              ></Sidebar>
+          </div>
         <JobBoardMainFeed />
       </div>
     );
