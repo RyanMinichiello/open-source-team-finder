f41e143a43953a5e6a1df786c850f45add4e0f0e
ryan-lecours <rlecours@umass.edu>
2017-04-10 16:36:48 -0400

    took out extra nav/sidebar html

--- app/components/createproject/createproject.js
+++ app/components/createproject/createproject.js
@@ -12,90 +12,10 @@ export default class CreateProject extends React.Component {
 
       <div>
 
-        <nav className="navbar navbar-default navbar-fixed-top">
-           NAVBAR
-        </nav>
-        <nav className="navbar navbar-fixed-top navbar-default">
-
-           <div className="container">
-
-              <div className="navbar-header">
-                 <button type="button" className="navbar-toggle collapsed" data-toggle="collapse"
-                    data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
-                 <span className="sr-only">Toggle navigation</span>
-                 <span className="icon-bar"></span>
-                 <span className="icon-bar"></span>
-                 <span className="icon-bar"></span>
-                 </button>
-
-                 <a className="navbar-brand" href="#">
-
-                    <span><img src="img/logo_notext.png" className= "logo pull-left" /></span>
-                 </a>
-              </div>
-
-              <div className="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-                 <form className="navbar-form navbar-left" role="search" >
-
-                    <div className="input-group">
-                       <input type="text fb-search" className="form-control" placeholder="Search OSTF" />
-                       <span className="input-group-btn" >
-                       <button type="submit" className="btn btn-default">
-                       <span className="glyphicon glyphicon-search"></span>
-                       </button>
-                       </span>
-                    </div>
-                 </form>
-               </div>
-                 <div className="nav navbar-nav navbar-right">
-
-                    <div className="btn-toolbar pull-right" role="toolbar">
-                       <div className="btn-group" role="group">
-                          <button type="button" className="btn btn-default navbar-btn">
-                          <span className="glyphicon glyphicon-user"></span>
-                          Jane
-                          </button>
-                       </div>
-                       <div className="btn-group" role="group">
-                          <button type="button" className="btn btn-default navbar-btn">
-                          <span className="glyphicon glyphicon-envelope"></span>
-                          </button>
-                       </div>
-                       <div className="btn-group" role="group">
-                          <button type="button" className="btn btn-default navbar-btn orange-btn">
-                          +Create Project
-                          </button>
-                       </div>
-                       <div className="btn-group" role="group">
-                          <div className="btn-group" role="group">
-                             <button type="button" className="btn btn-default navbar-btn">
-                             Sign Out
-                             </button>
-                          </div>
-                       </div>
-                    </div>
-                 </div>
-              </div>
 
 
 
-        </nav>
-
-        <div className="col-md-2 side-bar">
-
-           <ul className="nav nav-pills nav-stacked">
-              <li role="presentation">PROJECTS</li>
-              <li role="presentation"><a href="#"> Project 1 </a> </li>
-              <li role="presentation"><a href="#"> Project 2 </a> </li>
-              <li role="presentation"><a href="#"> Project 3 </a> </li>
-              <li role="presentation"><a href="#"> Project 4 </a> </li>
-              <li role="presentation"><a href="#"><span className="glyphicon glyphicon-stats"></span>
-                 Job Board</a>
-              </li>
-           </ul>
-           <div style="height: 1000px;"></div>
-        </div>
-
+      
         <Navbar
           self_name = "Jane"
           ></Navbar>
