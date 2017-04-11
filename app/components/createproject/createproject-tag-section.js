5d3a1354eeb24fbb8eaa87225a3655558adc99a0
ryan-lecours <rlecours@umass.edu>
2017-04-11 11:59:14 -0400

    added incomplete functionality to all buttons for createproject

--- app/components/createproject/createproject-tag-section.js
+++ app/components/createproject/createproject-tag-section.js
@@ -1,8 +1,20 @@
 import React from 'react';
 import TagItem from './createproject-tag-item.js'
 
-
+var inputs= [];
 export default class CreateProjectTagSection extends React.Component {
+  constructor(props) {
+
+        super(props);
+        this.state = { inputs };
+    }
+
+    addNewButton(){
+      var newInput = <createproject-tag-item />
+      inputs.push(newInput);
+      this.setState({inputs})
+    }
+
   render() {
     return (
 
@@ -12,7 +24,7 @@ export default class CreateProjectTagSection extends React.Component {
         <div className= "attribute-box">
          <div className= "attribute">Areas of Interest (5 Max):</div>
          <input placeholder="Add Tags" />
-           <button type="button" id="add-more-tags-btn" className="btn btn-default">
+           <button type="button" id="add-more-tags-btn" className="btn btn-default" onClick={ () => this.addNewButton() }>
            Add Tag
            </button>
            <TagItem />
