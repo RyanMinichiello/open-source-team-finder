01ec8c70fcbf6c3a3162dccb93307e829a49f785
Nicholas Achin <nicholasachi@umass.edu>
2017-04-11 16:33:47 -0400

    Database changes

--- app/database.js
+++ app/database.js
@@ -2,158 +2,166 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 var initialData = {
-    // The 'user' collection; contains all the users in our system
-    "users": {
-        // This user has id "1" and it is Jane
-        "1" : {
-            "_id" : 1,
-            "fullName" : "Jane Doe",
-            // ID of Jane's feed
-            "feed" : 1,
-            // ID of Jane's Inbox
-            "inbox" : 1,
-            "skills" : ["Scala", "Node.js", "Agile Methodology"],
-            "interests" : ["Finance", "Clean Energy"],
-            "bio" : "Hi my name is Jane",
-            "contact" : "myemail@email.com",
-            "projects" : ["1", "2"]
-        },
-        "2" : {
-            "_id" : 2,
-            "fullName" : "Joe Doe",
-            // ID of Jane's feed
-            "feed" : 2,
-            // ID of Jane's Inbox
-            "inbox" : 2,
-            "skills" : ["Java", "Numerical Graphing", "Agile Methodology", "Management"],
-            "interests" : ["Drones", "Robotics", "Sports"],
-            "bio" : "Joe, a deer, a female deer, Ray, a drop of golden sun, Me, a name, etc.",
-            "contact" : "JoesCarEmporiumComeOnDown@email.com",
-            "projects" : ["2", "3"]
-        }
+  // The 'user' collection; contains all the users in our system
+  "users": {
+    // This user has id "1" and it is Jane
+    "1" : {
+      "_id" : 1,
+      "fullName" : "Jane Doe",
+      // ID of Jane's feed
+      "feed" : 1,
+      // ID of Jane's Inbox
+      "inboxId" : 1,
+      "skills" : ["Scala", "Node.js", "Agile Methodology"],
+      "interests" : ["Finance", "Clean Energy"],
+      "bio" : "Hi my name is Jane",
+      "contact" : "myemail@email.com",
+      "projects" : ["1", "2"]
     },
-    "project" : {
-        "1":{
-          "identifier" : "Okra",
-          "id"         : 1,
-          "updates"    : 1,
-          "msgs"       : 1,
-          "pos"        : 1,
-          "description": "Welcome to the show",
-          "skillz"     :["Java", "Social", "Sick Card Tricks"]
 
-        },
-        "2" : {
-          "identifier" : "Apple",
-          "id"         : 2,
-          "updates"    : 2,
-          "msgs"       : 2,
-          "pos"        : 2,
-          "description": "Nothing too fancy to do here.",
-          "skillz"     :["Java", "Social", "Extreme Apple Picking", "High Level Design"]
-        },
-        "3" : {
-          "identifier" : "Peach",
-          "id"         : 3,
-          "updates"    : 3,
-          "msgs"       : 3,
-          "pos"        : 3,
-          "description": "We're looking for our friend James. We believe he is in the ocean and we need to develop good drones to find him.",
-          "skillz"     :["Drones", "Artificial Intelligence", "Node.js", "Mongo"]
-        },
-        "4" : {
-          "identifier" : "Garbanzo Bean",
-          "id"         : 4,
-          "updates"    : 4,
-          "msgs"       : 4,
-          "pos"        : 4,
-          "description": "Our goal is too develop an onlign spell cheker that finds any errands you type.",
-          "skillz"     :["Natural Language Processing", "Excel", "Java", "Regex"]
-        }
-    },
-    "inbox" : {
-        //null
-    },
-    "message" : {
+    "2" : {
+      "_id" : 2,
+      "fullName" : "Joe Doe",
+      // ID of Jane's feed
+      "feed" : 2,
+      // ID of Jane's Inbox
+      "inboxId" : 2,
+      "skills" : ["Java", "Numerical Graphing", "Agile Methodology", "Management"],
+      "interests" : ["Drones", "Robotics", "Sports"],
+      "bio" : "Joe, a deer, a female deer, Ray, a drop of golden sun, Me, a name, etc.",
+      "contact" : "JoesCarEmporiumComeOnDown@email.com",
+      "projects" : ["2", "3"]
+    }
+  },
+
+  "project" : {
+    "1":{
+      "identifier" : "Okra",
+      "id"         : 1,
+      "updates"    : 1,
+      "msgs"       : 1,
+      "pos"        : 1,
+      "description": "Welcome to the show",
+      "skillz"     :["Java", "Social", "Sick Card Tricks"]
 
     },
-    "positions" : {
-
-        "open_positions" :{
-
-          "2":{
-            "id"         : 2,
-            "project_id" : 1,
-            "status"     : "vacant",
-            "title"      : "developer",
-            "description": "develop things",
-            "skillz"     : ["Java"]
-          }
-        },
-
-        "filled_positions":{
-          "1":{
-            "id"         : 1,
-            "project_id" : 1,
-            "status"     : "filled",
-            "title"      : "developer",
-            "description": "develop things",
-            "skillz"     : ["Java"]
-          }
-        }
-
+    "2" : {
+      "identifier" : "Apple",
+      "id"         : 2,
+      "updates"    : 2,
+      "msgs"       : 2,
+      "pos"        : 2,
+      "description": "Nothing too fancy to do here.",
+      "skillz"     :["Java", "Social", "Extreme Apple Picking", "High Level Design"]
     },
-    "updates" : {
-
+    "3" : {
+      "identifier" : "Peach",
+      "id"         : 3,
+      "updates"    : 3,
+      "msgs"       : 3,
+      "pos"        : 3,
+      "description": "We're looking for our friend James. We believe he is in the ocean and we need to develop good drones to find him.",
+      "skillz"     :["Drones", "Artificial Intelligence", "Node.js", "Mongo"]
     },
-    "feeds" :{
+    "4" : {
+      "identifier" : "Garbanzo Bean",
+      "id"         : 4,
+      "updates"    : 4,
+      "msgs"       : 4,
+      "pos"        : 4,
+      "description": "Our goal is too develop an onlign spell cheker that finds any errands you type.",
+      "skillz"     :["Natural Language Processing", "Excel", "Java", "Regex"]
+    }
+  },
+  "inbox" : {
+    "1" : {
+      "chats": 1
+    }
+  },
 
+  "chats" : {
+    "1": {
+      "messages" : [1,2],
+      "associated-users" : [1,2]
+    }
+  },
+
+  "messages" : {
+    "1" : {
+      "contents" : "hqwertwertwert"
     },
-    "feed-items" : {
-      "job-items": {
-        "1" : {
-          "id" : 1,
-          "title" : "Software Engineering Position at Okra Team",
-          "description" : "apply within!!!!",
-          "tags" : ["Drones", "Finance"]
-        },
-        "2" : {
-          "id" : 2,
-          "title" : "Quality Assurance Position at Apple Team",
-          "description" : "Please taste our apples we are too afraid.",
-          "tags" : ["Apples", "Sports"]
-        }
+    "2" : {
+      "contents" : "alksdfasldhfhkjasd"
+    }
+  },
 
+  "positions" : {
+
+      "1":{
+        "id"         : 1,
+        "status" : "filled",
+        "userId" : 1,
+        "project_id" : 1,
+        "title"      : "developer",
+        "description": "develop things",
+        "skillz"     : ["Java"]
       },
 
-      "notification-items" : {
-        "1" : {
-          "id" : 1,
-          "title" : "Notification from Apple Team",
-          "description" : "Jane pleasae finish the mock UI today."
-        },
-        "2" : {
-          "id" : 2,
-          "title" : "Notification from Apple Team",
-          "description" : "We need a new CEO after Jim left. Send in recommendation for a potential bonus."
-        }
-      },
-
-      "update-items" : {
-        "1" : {
-          "id" : 1,
-          "title" : "Update on Garbanzo Bean Team",
-          "description" : "We are running out of bean steam and may be closing down in July :/."
-        },
-        "2" : {
-          "id" : 2,
-          "title" : "Update on Apple Team",
-          "description" : "Things couldn't be any better, but could we get more VC support?"
-        }
+      "2":{
+        "id"         : 1,
+        "status" : "open",
+        "userId" : 1,
+        "project_id" : 1,
+        "title"      : "developer",
+        "description": "develop things",
+        "skillz"     : ["Java"]
       }
 
+  },
 
+  "feeds" :{
+    //Each feed has their own set of feed items
+    "1" : {
+      "id": 1,
+      "notificationItems": [1],
+      "positions":[1,2]
+    },
+    "2" : {
+      "id": 2,
+      "positions": [1,2],
+      "notificationItems": [1]
     }
+  },
+
+  "jobItems": {
+    "1" : {
+      "id" : 1,
+      "title" : "Software Engineering Position at Okra Team",
+      "description" : "apply within!!!!",
+      "tags" : ["Drones", "Finance"]
+    },
+    "2" : {
+      "id" : 2,
+      "title" : "Quality Assurance Position at Apple Team",
+      "description" : "Please taste our apples we are too afraid.",
+      "tags" : ["Apples", "Sports"]
+    }
+
+  },
+
+  "notificationItems" : {
+    "1" : {
+      "id" : 1,
+      "title" : "Update on Garbanzo Bean Team",
+      "description" : "We are running out of bean steam and may be closing down in July :/."
+    },
+    "2" : {
+      "id" : 2,
+      "title" : "Update on Apple Team",
+      "description" : "Things couldn't be any better, but could we get more VC support?"
+    }
+  }
+
 };
 
 var data = JSON.parse(localStorage.getItem('facebook_data'));
@@ -162,18 +170,18 @@ if (data === null) {
 }
 
 /**
- * A dumb cloning routing. Serializes a JSON object as a string, then
- * deserializes it.
- */
+* A dumb cloning routing. Serializes a JSON object as a string, then
+* deserializes it.
+*/
 function JSONClone(obj) {
   return JSON.parse(JSON.stringify(obj));
 }
 
 /**
- * Emulates reading a "document" from a NoSQL database.
- * Doesn't do any tricky document joins, as we will cover that in the latter
- * half of the course. :)
- */
+* Emulates reading a "document" from a NoSQL database.
+* Doesn't do any tricky document joins, as we will cover that in the latter
+* half of the course. :)
+*/
 export function readDocument(collection, id) {
   // Clone the data. We do this to model a database, where you receive a
   // *copy* of an object and not the object itself.
@@ -181,8 +189,8 @@ export function readDocument(collection, id) {
 }
 
 /**
- * Emulates writing a "document" to a NoSQL database.
- */
+* Emulates writing a "document" to a NoSQL database.
+*/
 export function writeDocument(collection, changedDocument) {
   var id = changedDocument._id;
   // Store a copy of the object into the database. Models a database's behavior.
@@ -192,8 +200,8 @@ export function writeDocument(collection, changedDocument) {
 }
 
 /**
- * Adds a new document to the NoSQL database.
- */
+* Adds a new document to the NoSQL database.
+*/
 export function addDocument(collectionName, newDoc) {
   var collection = data[collectionName];
   var nextId = Object.keys(collection).length;
@@ -206,29 +214,29 @@ export function addDocument(collectionName, newDoc) {
 }
 
 /**
- * Reset our browser-local database.
- */
+* Reset our browser-local database.
+*/
 export function resetDatabase() {
   localStorage.setItem('facebook_data', JSON.stringify(initialData));
   data = JSONClone(initialData);
 }
 
 /**
- * Reset database button.
- */
+* Reset database button.
+*/
 class ResetDatabase extends React.Component {
   render() {
     return (
       <button className="btn btn-default" type="button" onClick={() => {
-        resetDatabase();
-        window.alert("Database reset! Refreshing the page now...");
-        document.location.reload(false);
-      }}>Reset Mock DB</button>
-    );
+          resetDatabase();
+          window.alert("Database reset! Refreshing the page now...");
+          document.location.reload(false);
+        }}>Reset Mock DB</button>
+      );
+    }
   }
-}
 
-// ReactDOM.render(
-//   <ResetDatabase />,
-//   document.getElementById('fb-db-reset')
-// );
+  // ReactDOM.render(
+  //   <ResetDatabase />,
+  //   document.getElementById('fb-db-reset')
+  // );
