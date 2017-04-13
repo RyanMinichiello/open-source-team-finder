7c7b35c6a49b3dc3f1b11406761950d630a4aa6c
ryanminichiello <ryan.minichiello@gmail.com>
2017-04-13 12:43:36 -0400

    dynamish profile, untested with broken sidebar

--- app/components/profile-page/profile_skills.js
+++ app/components/profile-page/profile_skills.js
@@ -1,31 +1,33 @@
 import React from 'react';
-import ProfileSkillItem from './profile_skill_item.js'
+import ProfileButton from './profile_button.js'
 
 export default class Profile_Skills extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            contents : [],
-            pid      : 1
-        };
-    }
-
-    generateSkills(list) {
-        if(list) {
-            return list.map(this.createSkill);
+        constructor(props) {
+            super(props);
+            this.state = {
+                contents : [],
+                pid : 1
+            };
         }
-    }
 
-    createSkill(item) {
-        return <ProfileSkillItem key={item} skill = {item}/>
-    }
+        generateButtonGroup(list) {
+            if(list) {
+                return list.map(this.createButton);
+            }
+        }
 
-    refresh() {
-        this.setState({pid: this.props.profileID});
-    }
+        createButton(item) {
+            return <ProfileButton key={item} skill = {item}/>
+        }
+        refresh() {
+            /*TODO*/
+        }
 
+        coponentDidMount() {
+            this.refresh();
+        }
 
-    render() {
+       render() {
         return (
             <div>
             <div className="profile-skills col-md-4 mid-col">
@@ -33,10 +35,9 @@ export default class Profile_Skills extends React.Component {
                   {this.props.name}
               </h3>
               <h3>Skills</h3>
-                {this.generateSkills(this.props.skills)}
-                <br/><br/>
-              /*<div className="btn-group">
-                  <button type="button" className="btn btn-skill-interest" >
+              <div className="btn-group">
+                  {this.generateButtonGroup(this.props.skills)}
+                  /*<button type="button" className="btn btn-skill-interest" >
                   {this.props.first_skill}
                   </button>
                   <button type="button" className="btn btn-skill-interest  " >
@@ -47,16 +48,17 @@ export default class Profile_Skills extends React.Component {
                   </button>
                   <button type="button" className="btn btn-skill-interest  " >
                   {this.props.fourth_skill}
-                  </button>
-              </div>*/
+                  </button>*/
+              </div>
               <h3>Interests</h3>
               <div className="btn-group">
-                  <button type="button" className="btn btn-skill-interest  " >
+                  {this.generateButtonGroup(this.props.interests)}
+                  /*<button type="button" className="btn btn-skill-interest  " >
                   {this.props.first_interest}
                   </button>
                   <button type="button" className="btn btn-skill-interest  " >
                   {this.props.second_interest}
-                  </button>
+                  </button>*/
               </div>
             </div>
         </div>
