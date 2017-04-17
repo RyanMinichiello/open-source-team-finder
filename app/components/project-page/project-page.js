bed9c116a4be5a65d040e734cbf7cb9bdcf7524b
ryanminichiello <ryan.minichiello@gmail.com>
2017-04-17 10:05:43 -0400

    fixed tom's page cuz pretty sure I broke it

--- app/components/project-page/project-page.js
+++ app/components/project-page/project-page.js
@@ -3,8 +3,9 @@ import ProjectBanner from './project-banner';
 import ProjectDescription from './project-description';
 import ProjectMessages from './project-messages';
 import ProjectPositions from './project-positions';
+import Sidebar from '../sidebar.js';
+import Navbar from '../navbar.js';
 import {getProjectData} from '../../server';
-// import Navbar from '../navbar.js';
 
 export default class ProjectPage extends React.Component {
   constructor(props) {
@@ -28,13 +29,20 @@ export default class ProjectPage extends React.Component {
 
   render(){
     return(
-        <div className="project-container row">
+        <div>
+        <Navbar></Navbar>
+            <div className="row"></div>
+                <Sidebar
+                ></Sidebar>
+            <div className = "col-md-2"></div>
+        <div className="project-zone col-md-10">
             <ProjectBanner uid={this.props.uid} pid={this.props.pid}/>
             <div className="project-banner"><h1 className="banner-header">{this.state.identifier}</h1></div>
             <ProjectDescription projectDescription={this.state.description} projectSkillz={this.state.skillz} positionID={this.state.pos}/>
             <ProjectMessages updateID={this.state.updates}/>
             <ProjectPositions positionID={this.state.pos}/>
         </div>
+    </div>
     )
   }
 }
