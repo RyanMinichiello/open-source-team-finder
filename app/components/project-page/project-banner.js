3905d8c28164fbc8f1d2af423b8892dd42a6ae33
lookup828 <tcloutier@umass.edu>
2017-04-19 17:49:50 -0400

    Project Page Update

--- app/components/project-page/project-banner.js
+++ app/components/project-page/project-banner.js
@@ -2,14 +2,27 @@ import React from 'react';
 
 export default class ProjectBanner extends React.Component {
 
+  constructor(props) {
+     super(props);
+     this.state = {
+        contents : [],
+        applied  : 0
+      };
+   }
   onApply(){
-    console.log("Hello");
+    this.setState({applied : 1});
   }
 
   getButtons(){
     //change to if they are an admin have edit button show up
+    var button;
+    if(this.state.applied == 0){
+      button =<button className="header-button" onClick={(e) => this.onApply(e)} >Apply</button>
+    }else {
+      button =<button className="header-button"> Applied <span className="glyphicon glyphicon-ok"></span></button>
+    }
     return (<div className="header-buttons">
-      <button className="header-button" onClick={(e) => this.onApply(e)} >Apply</button>
+      {button}
     </div>);
   }
   render(){
