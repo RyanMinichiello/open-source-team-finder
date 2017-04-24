a9be069823a38b68143189f8ef3a86e8ee0692a6
Nicholas Achin <nicholasachi@umass.edu>
2017-04-24 18:48:06 -0400

    Fixed CSS on Main Feed

--- app/components/main-feed/main-feed-job-item.js
+++ app/components/main-feed/main-feed-job-item.js
@@ -5,25 +5,27 @@ export default class MainFeedJobItem extends React.Component {
   render() {
     return (
       <div>
-        <div className="panel-body job-post">
-          <div className="job-title">
-            <h3>
-              <span className="glyphicon glyphicon-paperclip"></span>
-              <a href='#'> {this.props.feedItemName}</a>
-            </h3>
-          </div>
-          <div className="job-desc">
-            <p>
-               {this.props.postData}
-            </p>
-          </div>
-          <div className="row">
-            <MainFeedTagList/>
-            <div className="col-md-6">
-              <div className="pull-right">
-                Ranking: <button type="button" className={this.props.rankingType} >
-                <span><img src="img/favicon.ico" className="best-fit-ico"/></span>
-                </button>
+        <div className="panel">
+          <div className="panel-body job-post">
+            <div className="job-title">
+              <h3>
+                <span className="glyphicon glyphicon-paperclip"></span>
+                <a href='#'> {this.props.feedItemName}</a>
+              </h3>
+            </div>
+            <div className="job-desc">
+              <p>
+                 {this.props.postData}
+              </p>
+            </div>
+            <div className="row">
+              <MainFeedTagList/>
+              <div className="col-md-6">
+                <div className="pull-right">
+                  Ranking: <button type="button" className={this.props.rankingType} >
+                  <span><img src="img/favicon.ico" className="best-fit-ico"/></span>
+                  </button>
+                </div>
               </div>
             </div>
           </div>
--- app/components/main-feed/main-feed-notification-item.js
+++ app/components/main-feed/main-feed-notification-item.js
@@ -4,20 +4,22 @@ export default class MainFeedNotificationItem extends React.Component {
   render() {
     return (
       <div>
-        <div className="panel-body job-post">
-          <div className="job-title">
-            <h3>
-              <span className="glyphicon glyphicon-eye-open"></span>
-              <a href='#'> {this.props.feedItemName}</a>
-            </h3>
-          </div>
-          <div className="job-desc">
-            <p>
-              {this.props.postData}
-            </p>
-          </div>
-          <div className="row">
+        <div className="panel" >
+          <div className="panel-body job-post">
+            <div className="job-title">
+              <h3>
+                <span className="glyphicon glyphicon-eye-open"></span>
+                <a href='#'> {this.props.feedItemName}</a>
+              </h3>
+            </div>
+            <div className="job-desc">
+              <p>
+                {this.props.postData}
+              </p>
+            </div>
+            <div className="row">
 
+            </div>
           </div>
         </div>
       </div>
