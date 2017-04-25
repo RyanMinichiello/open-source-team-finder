4e52dd821a2cc12b0dbc86cfba23e047d6cfd2d0
Nicholas Achin <nicholasachi@umass.edu>
2017-04-24 20:57:11 -0400

    Fixed filter bar and re routed main-feed to landing page

--- app/components/main-feed/main-feed-filters.js
+++ app/components/main-feed/main-feed-filters.js
@@ -5,15 +5,13 @@ export default class MainFeedFilter extends React.Component {
     return (
       <div>
         <div className="row navbar main-feed-header">
-          <h6> FILTERS: </h6>
           <div className="checkbox">
-            <span className="glyphicon glyphicon-paperclip"></span>
+            <h12> Job Openings <span className="glyphicon glyphicon-paperclip"></span> </h12>
           </div>
+
           <div className="checkbox">
-            <span className="glyphicon glyphicon-folder-open"></span>
-          </div>
-          <div className="checkbox">
-            <span className="glyphicon glyphicon-eye-open"></span>
+            <h6> Team Updates <span className="glyphicon glyphicon-eye-open"></span> </h6>
+
           </div>
         </div>
       </div>
